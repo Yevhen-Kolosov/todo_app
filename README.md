0075aa99484a6d3d6c6ac3eaaa10dae8ec5edec2
Yevhen Kolosov <kolosov214a@gmail.com>
2024-02-11 22:43:06 +0200

    clean up

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
-# React TypeScript Starter Pack
+# React Todo App
 
-To use this template click `Use this template`
+- [DEMO LINK](https://yevhen-kolosov.github.io/todo_app/)
 
-### Available Scripts
-
-`Deploy` - available to deploy your application to gh-pages
-
-`SCSS Preprocessor` - available to write your styles with modern style language
+## Description:
+1. Loads todos from the server (currently the todos are shared and not unique to the user);
+1. All the changes will be saved to the API;
+1. In case of any error shows the notification with an appropriate message;
+    - you can close the notification with the `close` button;
+    - it will be automatically hidden in 3 seconds;
+1. Add a todo(write title and press `Enter`);
+1. Toggle the `completed` status by click on checkbox for current todo;
+1. Toggle the `completed` status of all the todos with the toggle button.
+1. Edit todo title on double click;
+    - save changes on the form submit (just press `Enter` or click in another place);
+    - cancel editing by pressing key `Esc`;
+    - if the new title is empty the todo will be deleted;
+1. Filter todos by click on buttons `All` / `Active` / `Completed`
--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,279 @@
-import React from 'react';
-import './App.scss';
+import React, {
+  Dispatch,
+  FormEvent,
+  SetStateAction,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+import { TodoMain } from './components/TodoMain';
+import { TodoHeader } from './components/TodoHeader';
+import { FilterType } from './types/FilterType';
+import { TodoNotification } from './components/TodoNotification';
+import { TodoFooter } from './components/TodoFooter';
+import { Todo } from './types/Todo';
+import {
+  addTodo,
+  deleteTodo,
+  getTodos,
+  patchTodo,
+} from './api/todos';
+import { ErrorType } from './types/ErrorType';
+import { filterTodos } from './utils/filterTodos';
 
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
+const USER_ID = 12031;
 
 export const App: React.FC = () => {
+  const [
+    selectedFilter,
+    setSelectedFilter,
+  ] = useState<FilterType>(FilterType.All);
+
+  const [todosFromServer, setTodosFromServer] = useState<Todo[]>([]);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [selectedTodoIds, setSelectedTodoIds] = useState<number[]>([]);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [
+    currentError,
+    setCurrentError,
+  ] = useState<ErrorType | null>(null);
+
+  const timerId = useRef<NodeJS.Timeout>();
+
+  const onError = (error: ErrorType) => {
+    if (timerId.current) {
+      clearTimeout(timerId.current);
+    }
+
+    setCurrentError(error);
+    timerId.current = setTimeout(
+      () => setCurrentError(null),
+      3000,
+    );
+  };
+
+  const handleSubmit = async (
+    event: FormEvent<HTMLFormElement>,
+    newTodoText: string,
+    setNewTodoText: Dispatch<SetStateAction<string>>,
+  ) => {
+    event.preventDefault();
+
+    if (!newTodoText.trim()) {
+      onError(ErrorType.EmptyTitle);
+
+      return;
+    }
+
+    const newTodo = {
+      id: 0,
+      userId: USER_ID,
+      title: newTodoText.trim(),
+      completed: false,
+    };
+
+    try {
+      setTempTodo(newTodo);
+      const todo = await addTodo(newTodo);
+
+      setTodosFromServer(
+        todos => ([...todos, todo]),
+      );
+
+      setNewTodoText('');
+    } catch {
+      onError(ErrorType.UnableToAdd);
+    } finally {
+      setTempTodo(null);
+
+      setTimeout(() => {
+        if (inputRef.current) {
+          inputRef.current.focus();
+        }
+      }, 100);
+    }
+  };
+
+  const handleSingleTodoDelition = async (todoId: number) => {
+    try {
+      setSelectedTodoIds(ids => [...ids, todoId]);
+
+      await deleteTodo(todoId);
+
+      setTodosFromServer(
+        todos => todos.filter(todo => todo.id !== todoId),
+      );
+    } catch {
+      onError(ErrorType.UnableToDelete);
+    } finally {
+      setTimeout(
+        () => {
+          setSelectedTodoIds(
+            ids => ids.filter(id => !(id === todoId)),
+          );
+
+          if (inputRef.current) {
+            inputRef.current.focus();
+          }
+        },
+        300,
+      );
+    }
+  };
+
+  const completedTodoIds = todosFromServer
+    .filter(todo => todo.completed)
+    .map(todo => todo.id);
+
+  const handleCompletedTodosDelition = async () => {
+    try {
+      setSelectedTodoIds(ids => [...ids, ...completedTodoIds]);
+
+      await Promise.all(completedTodoIds.map(async (todoId) => {
+        try {
+          await deleteTodo(todoId);
+          setTodosFromServer(
+            todos => todos.filter(todo => !(todo.id === todoId)),
+          );
+        } catch {
+          onError(ErrorType.UnableToDelete);
+        }
+      }));
+    } finally {
+      setTimeout(
+        () => {
+          setSelectedTodoIds(
+            ids => ids.filter(id => !completedTodoIds.includes(id)),
+          );
+
+          if (inputRef.current) {
+            inputRef.current.focus();
+          }
+        },
+        300,
+      );
+    }
+  };
+
+  const updateTodo = async (updatedTodo: Todo) => {
+    try {
+      setSelectedTodoIds(ids => [...ids, updatedTodo.id]);
+
+      const todoFromResponse = await patchTodo(updatedTodo);
+
+      setTodosFromServer(
+        todos => todos.map(td => {
+          if (td.id === todoFromResponse.id) {
+            return todoFromResponse;
+          }
+
+          return td;
+        }),
+      );
+    } catch {
+      onError(ErrorType.UnableToUpdate);
+      throw new Error();
+      // ^ this error throw is needed to trigger catch
+      // in onSubmitTitle (TodoItem.tsx)
+      // "Edit Form √ should stay open on fail"
+    } finally {
+      setSelectedTodoIds(
+        ids => ids.filter(id => !(id === updatedTodo.id)),
+      );
+    }
+  };
+
+  useEffect(() => {
+    setCurrentError(null);
+    getTodos(USER_ID)
+      .then(setTodosFromServer)
+      .catch(() => (onError(ErrorType.UnableToLoad)));
+  }, []);
+
+  const filteredTodos = useMemo(
+    () => filterTodos(todosFromServer, selectedFilter),
+    [todosFromServer, selectedFilter],
+  );
+
+  const activeTodos = todosFromServer.filter(todo => !todo.completed);
+  const activeTodoIds = activeTodos.map(todo => todo.id);
+  const activeTodosCount = activeTodos.length;
+
+  const handleToggleAll = async () => {
+    if (activeTodosCount) {
+      setSelectedTodoIds(ids => [...ids, ...activeTodoIds]);
+
+      await Promise.all(activeTodos.map(async (todo) => {
+        try {
+          const updatedTodo = {
+            ...todo,
+            completed: true,
+          };
+
+          await updateTodo(updatedTodo);
+        } catch {
+          onError(ErrorType.UnableToUpdate);
+        }
+      }));
+    } else {
+      setSelectedTodoIds(ids => [...ids, ...completedTodoIds]);
+
+      await Promise.all(todosFromServer.map(async (todo) => {
+        try {
+          const updatedTodo = {
+            ...todo,
+            completed: false,
+          };
+
+          await updateTodo(updatedTodo);
+        } catch {
+          onError(ErrorType.UnableToUpdate);
+        }
+      }));
+    }
+  };
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
-        TodoList
-      </Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <TodoHeader
+          isTodoListEmpty={!todosFromServer.length}
+          isAnyTodoActive={!!activeTodosCount}
+          isNewLoading={!!tempTodo}
+          handleSubmit={handleSubmit}
+          handleToggleAll={handleToggleAll}
+          inputRef={inputRef}
+        />
+
+        <TodoMain
+          todos={filteredTodos}
+          tempTodo={tempTodo}
+          selectedTodoIds={selectedTodoIds}
+          onDelete={handleSingleTodoDelition}
+          updateTodo={updateTodo}
+        />
+
+        {(todosFromServer.length !== 0 || tempTodo) && (
+          <TodoFooter
+            activeTodosCount={activeTodosCount}
+            isAnyTodoCompleted={activeTodosCount !== todosFromServer.length}
+            selectedFilter={selectedFilter}
+            setSelectedFilter={setSelectedFilter}
+            onClearCompleted={handleCompletedTodosDelition}
+          />
+        )}
+      </div>
+
+      <TodoNotification
+        currentError={currentError}
+        setCurrentError={setCurrentError}
+      />
     </div>
   );
 };
